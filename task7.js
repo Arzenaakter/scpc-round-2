4b9d3ebfbca4cb8b0b513b1d7cc694b74d8919d7
Arzena Akter <arzenaakter@gmail.com>
2023-07-27 15:41:04 +0600

    task 7 done

--- task7.js
+++ task7.js
@@ -1,13 +1,13 @@
 // Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
-function findSecondSmallest(arr) {
-  if (!Array.isArray(arr) || arr.length < 2) {
+function findSecondSmallest(inputArray) {
+  if (!Array.isArray(inputArray) || inputArray.length < 2) {
     return "Input should be an array with at least two elements.";
   }
 
-  const sortedArray = arr.sort((a, b) => a - b);
+  const sortedArray = inputArray.sort((a, b) => a - b);
 
   return sortedArray[1];
 }
-const numbers = [5, 0, 8, 1, 2, 9, 3];
+const numbers = [5, 0, 8, 4, 2, 9, 3];
 const secondSmallest = findSecondSmallest(numbers);
 console.log("Second Smallest:", secondSmallest);
